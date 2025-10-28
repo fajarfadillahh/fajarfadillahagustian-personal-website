cff2f4311f780b719b5df23dc3752cbbe45740d9
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-10-28 22:15:02 +0700

    feat: add animation testimonials section

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -1,67 +1,86 @@
 "use client";
 
+import Testimonial from "@/components/Testimonial";
 import { testimonials } from "@/data/site";
 import { ArrowLeftIcon, ArrowRightIcon } from "@phosphor-icons/react";
-import Image from "next/image";
+import { AnimatePresence, motion, useScroll, useTransform } from "motion/react";
+import { useRef, useState } from "react";
 
 export default function SectionTestimonials() {
-  const testiIndex = 0;
+  const [testimonialIndex, setTestimonialIndex] = useState(0);
+
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: titleRef,
+    offset: ["start end", "end start"],
+  });
+  const transformTop = useTransform(scrollYProgress, [0, 1], ["0%", "15%"]);
+  const transformBottom = useTransform(scrollYProgress, [0, 1], ["0%", "-15%"]);
+
+  const handleClickPrev = () => {
+    setTestimonialIndex((curr) => {
+      if (curr === 0) return testimonials.length - 1;
+
+      return curr - 1;
+    });
+  };
+
+  const handleClickNext = () => {
+    setTestimonialIndex((curr) => {
+      if (curr === testimonials.length - 1) return 0;
+
+      return curr + 1;
+    });
+  };
 
   return (
     <section id="testimonials" className="section">
-      <h2 className="flex flex-col overflow-hidden text-4xl md:text-7xl lg:text-8xl">
-        <span className="whitespace-nowrap">
+      <h2
+        className="flex flex-col overflow-hidden text-4xl md:text-7xl lg:text-8xl"
+        ref={titleRef}
+      >
+        <motion.span
+          className="whitespace-nowrap"
+          style={{
+            x: transformTop,
+          }}
+        >
           Some nice words from my past clients
-        </span>
-        <span className="self-end whitespace-nowrap text-orange-500">
+        </motion.span>
+        <motion.span
+          className="self-end whitespace-nowrap text-orange-500"
+          style={{
+            x: transformBottom,
+          }}
+        >
           Some nice words from my past clients
-        </span>
+        </motion.span>
       </h2>
 
       <div className="container">
         <div className="mt-20">
-          {testimonials.map(
-            (testi, index) =>
-              index === testiIndex && (
-                <div
-                  key={index}
-                  className="grid md:grid-cols-5 md:items-center md:gap-8 lg:gap-16"
-                >
-                  <div className="aspect-square md:col-span-2 md:aspect-9/16">
-                    <Image
-                      src={testi.image}
-                      alt={testi.name}
-                      width={390}
-                      height={694}
-                      className="size-full object-cover"
-                      style={{
-                        objectPosition: `50% ${testi.imagePositionY * 100}%`,
-                      }}
-                    />
-                  </div>
-
-                  <blockquote className="md:col-span-3">
-                    <div className="mt-8 text-3xl md:mt-0 md:text-5xl lg:text-6xl">
-                      <span>&ldquo;</span>
-                      <span>{testi.quote}</span>
-                      <span>&rdquo;</span>
-                    </div>
-
-                    <cite className="mt-4 block not-italic md:mt-8 md:text-lg lg:text-lg">
-                      {testi.name}, {testi.role} at {testi.company}
-                    </cite>
-                  </blockquote>
-                </div>
-              ),
-          )}
+          <AnimatePresence mode="wait" initial={false}>
+            {testimonials.map(
+              (testimonial, index) =>
+                index === testimonialIndex && (
+                  <Testimonial key={index} {...testimonial} />
+                ),
+            )}
+          </AnimatePresence>
         </div>
 
         <div className="mt-6 flex items-center gap-4 lg:mt-10">
-          <button className="inline-flex size-11 items-center justify-center rounded-full border border-stone-400">
+          <button
+            className="inline-flex size-11 items-center justify-center rounded-full border border-stone-400 transition-all duration-300 hover:cursor-pointer hover:border-orange-600 hover:bg-orange-500 hover:text-white"
+            onClick={handleClickPrev}
+          >
             <ArrowLeftIcon size={24} />
           </button>
 
-          <button className="inline-flex size-11 items-center justify-center rounded-full border border-stone-400">
+          <button
+            className="inline-flex size-11 items-center justify-center rounded-full border border-stone-400 transition-all duration-300 hover:cursor-pointer hover:border-orange-600 hover:bg-orange-500 hover:text-white"
+            onClick={handleClickNext}
+          >
             <ArrowRightIcon size={24} />
           </button>
         </div>
